d10faba425e0edaf47fa4b7c14841ec9b049610d
Ian Jay Calama-an <122774118+Ianjay0919@users.noreply.github.com>
2023-12-12 15:39:07 +0800

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -53,9 +53,80 @@ function Pricing() {
               <b><li><del>Expert setup & Track projects on the go</del></li></b>
               <b><li><del>Personal HR advisor</del></li></b>
             </ul>
-            <div class="sign-up">
-              <a href="#" class="btn bordered radius">Choose this Plan</a>
-            </div>
+                                        <div class="sign-up">
+                                <a href="#" class="btn bordered radius launch" data-toggle="modal" data-target="#staticBackdrop">Upgrade Plan</a>
+                                    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true"> 
+                                        <div class="modal-dialog"> 
+                                            <div class="modal-content"> 
+                                                <div class="modal-body"> 
+                                                    <div class="text-right"> 
+                                                        <i class="close" data-dismiss="modal">
+                                                            <span class="material-symbols-outlined">close</span>
+                                                        </i>
+                                                    </div> 
+                                                    <div class="tabs mt-3"> 
+                                                        <ul class="nav nav-tabs" id="myTab" role="tablist"> 
+                                                            <li class="nav-item" role="presentation"> 
+                                                                <a class="nav-link active" id="visa-tab" data-toggle="tab" href="#visa" role="tab" aria-controls="visa" aria-selected="true"> 
+                                                                    <img src="https://i.imgur.com/sB4jftM.png" width="80"/> 
+                                                                </a> 
+                                                            </li> 
+                                                            <li class="nav-item" role="presentation"> 
+                                                                <a class="nav-link" id="paypal-tab" data-toggle="tab" href="#paypal" role="tab" aria-controls="paypal" aria-selected="false"> 
+                                                                    <img src="https://i.imgur.com/yK7EDD1.png" width="80"/> 
+                                                                </a>
+                                                            </li> 
+                                                        </ul> 
+                                                        <div class="tab-content" id="myTabContent"> 
+                                                            <div class="tab-pane fade show active" id="visa" role="tabpanel" aria-labelledby="visa-tab"> 
+                                                                <div class="mt-4 mx-4"> 
+                                                                    <div class="text-center"> 
+                                                                        <h5>Credit card</h5> 
+                                                                    </div> 
+                                                                    <div class="form mt-3"> 
+                                                                        <div class="inputbox"> 
+                                                                            <input type="text" name="name" class="form-control" required="required"/> 
+                                                                            <span>Cardholder Name</span> 
+                                                                        </div> 
+                                                                        <div class="inputbox"> 
+                                                                            <input type="text" name="name" min="1" max="999" class="form-control" required="required"/> 
+                                                                            <span>Card Number</span>                                  
+                                                                        </div> 
+                                                                        <div class="d-flex flex-row"> 
+                                                                            <div class="inputbox"> 
+                                                                                <input type="text" name="name" min="1" max="999" class="form-control" required="required"/> 
+                                                                                <span>Expiration Date</span> 
+                                                                            </div> 
+                                                                            <div class="inputbox"> 
+                                                                                <input type="text" name="name" min="1" max="999" class="form-control" required="required"/> 
+                                                                                <span>CVV</span> 
+                                                                            </div> 
+                                                                        </div> 
+                                                                        <div class="px-5 pay"> 
+                                                                            <button class="btn btn-success btn-block">Pay Now</button> 
+                                                                        </div> 
+                                                                    </div> 
+                                                                </div> 
+                                                            </div> 
+                                                            <div class="tab-pane fade" id="paypal" role="tabpanel" aria-labelledby="paypal-tab"> 
+                                                                <div class="px-5 mt-5"> 
+                                                                    <div class="inputbox"> 
+                                                                        <input type="text" name="name" class="form-control" required="required"/> 
+                                                                        <span>Paypal Email Address</span>  
+                                                                    </div> 
+                                                                    <div class="pay px-5"> 
+                                                                        <button class="btn btn-primary btn-block">Add paypal</button> 
+                                                                    </div> 
+                                                                </div> 
+                                                            </div> 
+                                                        </div> 
+                                                    </div> 
+                                                </div> 
+                                            </div> 
+                                        </div>
+                                    </div>
+                                </div>
+
           </div>
         </div>
                   
@@ -79,9 +150,80 @@ function Pricing() {
               <b><li>Track time and projects on the go</li></b>
               <b><li>Personal HR advisor</li></b>
             </ul>
-            <div class="sign-up">
-              <a href="#" class="btn bordered radius">Choose this Plan</a>
-            </div>
+                                        <div class="sign-up">
+                                <a href="#" class="btn bordered radius launch" data-toggle="modal" data-target="#staticBackdrop">Upgrade Plan</a>
+                                    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true"> 
+                                        <div class="modal-dialog"> 
+                                            <div class="modal-content"> 
+                                                <div class="modal-body"> 
+                                                    <div class="text-right"> 
+                                                        <i class="close" data-dismiss="modal">
+                                                            <span class="material-symbols-outlined">close</span>
+                                                        </i>
+                                                    </div> 
+                                                    <div class="tabs mt-3"> 
+                                                        <ul class="nav nav-tabs" id="myTab" role="tablist"> 
+                                                            <li class="nav-item" role="presentation"> 
+                                                                <a class="nav-link active" id="visa-tab" data-toggle="tab" href="#visa" role="tab" aria-controls="visa" aria-selected="true"> 
+                                                                    <img src="https://i.imgur.com/sB4jftM.png" width="80"/> 
+                                                                </a> 
+                                                            </li> 
+                                                            <li class="nav-item" role="presentation"> 
+                                                                <a class="nav-link" id="paypal-tab" data-toggle="tab" href="#paypal" role="tab" aria-controls="paypal" aria-selected="false"> 
+                                                                    <img src="https://i.imgur.com/yK7EDD1.png" width="80"/> 
+                                                                </a>
+                                                            </li> 
+                                                        </ul> 
+                                                        <div class="tab-content" id="myTabContent"> 
+                                                            <div class="tab-pane fade show active" id="visa" role="tabpanel" aria-labelledby="visa-tab"> 
+                                                                <div class="mt-4 mx-4"> 
+                                                                    <div class="text-center"> 
+                                                                        <h5>Credit card</h5> 
+                                                                    </div> 
+                                                                    <div class="form mt-3"> 
+                                                                        <div class="inputbox"> 
+                                                                            <input type="text" name="name" class="form-control" required="required"/> 
+                                                                            <span>Cardholder Name</span> 
+                                                                        </div> 
+                                                                        <div class="inputbox"> 
+                                                                            <input type="text" name="name" min="1" max="999" class="form-control" required="required"/> 
+                                                                            <span>Card Number</span>                                  
+                                                                        </div> 
+                                                                        <div class="d-flex flex-row"> 
+                                                                            <div class="inputbox"> 
+                                                                                <input type="text" name="name" min="1" max="999" class="form-control" required="required"/> 
+                                                                                <span>Expiration Date</span> 
+                                                                            </div> 
+                                                                            <div class="inputbox"> 
+                                                                                <input type="text" name="name" min="1" max="999" class="form-control" required="required"/> 
+                                                                                <span>CVV</span> 
+                                                                            </div> 
+                                                                        </div> 
+                                                                        <div class="px-5 pay"> 
+                                                                            <button class="btn btn-success btn-block">Pay Now</button> 
+                                                                        </div> 
+                                                                    </div> 
+                                                                </div> 
+                                                            </div> 
+                                                            <div class="tab-pane fade" id="paypal" role="tabpanel" aria-labelledby="paypal-tab"> 
+                                                                <div class="px-5 mt-5"> 
+                                                                    <div class="inputbox"> 
+                                                                        <input type="text" name="name" class="form-control" required="required"/> 
+                                                                        <span>Paypal Email Address</span>  
+                                                                    </div> 
+                                                                    <div class="pay px-5"> 
+                                                                        <button class="btn btn-primary btn-block">Add paypal</button> 
+                                                                    </div> 
+                                                                </div> 
+                                                            </div> 
+                                                        </div> 
+                                                    </div> 
+                                                </div> 
+                                            </div> 
+                                        </div>
+                                    </div>
+                                </div>
+
           </div>
         </div>
             
